b0218262fd227ba123ce9953ccd0bd778c069574
AleksSG <samuilov.aleks@gmail.com>
2020-11-29 01:32:27 +0100

    Loaded file successfully

--- src/modules/landing-page/containers/LandingPageContainer.js
+++ src/modules/landing-page/containers/LandingPageContainer.js
@@ -1,8 +1,9 @@
-import React, { } from 'react';
+import React from 'react';
 
 import GCodeLoader from 'three-gcode-loader';
-//import { GCodeLoader } from '../../three/GCodeLoader';
-//import { OrbitControls } from '../../three/OrbitControls';
+
+//import { GCodeLoader } from "three/examples/jsm/loaders/GCodeLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as THREE from "three";
 
 import UploadButtonComponent from '../components/UploadButtonComponent';
@@ -22,38 +23,57 @@ const LandingPageContainer = () => {
 
     // On file load
     const onFileLoad = () => {
-        let camera, scene, renderer;
-        const container = document.createElement('div');
-        document.body.appendChild(container);
-        camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-        camera.position.set(0, 0, 70);
 
-        scene = new THREE.Scene();
+        const renderer = new THREE.WebGLRenderer();
+        // renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
 
+        const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
+        camera.position.set(90, 100, 70);
+        camera.lookAt(0, 0, 0);
+
+        const scene = new THREE.Scene();
+        scene.background = new THREE.Color( 0x61ff00 );
+        const material = new THREE.LineBasicMaterial({ color: 0x61ff00 });
+        const geometry = new THREE.BufferGeometry();
 
         loader.load(localStorage.getItem("fileGcode"), object => {
-            console.log(object[0]);
+            console.log(object[0][20][13]);
+
+            const positions = [];
+            object[0].forEach(layer => {
+                layer.forEach(linee => {
+                    linee.forEach(point => {
+                        if (point) {
+                            positions.push(new THREE.Vector3(point.x, point.y, point.z));
+                        }
+                    });
+                });
+            });
+            geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( positions, 3 ) );
+            
+
+            const line = new THREE.Line(geometry, material);
+            scene.add(line);
+
+            renderer.render(scene, camera);
         });
 
         // ############ START THREE JS LOADER
 
         // loader.load(localStorage.getItem("fileGcode"), object => {
-        //     object.position.set(0, 0, 0);
+        //     object.position.set( - 100, - 20, 100 );
+        //     console.log(object)
         //     scene.add(object);
         //     renderer.render(scene, camera);
         // });
 
-        renderer = new THREE.WebGLRenderer();
-        renderer.setPixelRatio(window.devicePixelRatio);
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        container.appendChild(renderer.domElement);
+        const controls = new OrbitControls(camera, renderer.domElement);
+        controls.addEventListener( 'change', () => renderer.render(scene, camera) ); // use if there is no animation loop
+        controls.minDistance = 10;
+        controls.maxDistance = 100;
 
-        // const controls = new OrbitControls(camera, renderer.domElement);
-        // controls.addEventListener('change', renderer.render(scene, camera)); // use if there is no animation loop
-        // controls.minDistance = 10;
-        // controls.maxDistance = 100;
-
-        renderer.render(scene, camera);
     };
 
     const getBase64 = file => new Promise((resolve, reject) => {
